2c1c05bb4d6bad0ba6fdc47ecdd51222592649fc
田昊冬 <xgpsthd0902@outlook.com>
2022-03-19 17:43:35 +0800

    Feat random shuffle

--- CodeCraft-2022.cpp
+++ CodeCraft-2022.cpp
@@ -1,40 +1,53 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
 #include <fstream>
 #include <iostream>
+#include <random>
 #include <sstream>
 
 using namespace std;
+
+const bool LOCAL_DEBUG = true;
 
 void readDemand();
 void readCap();
 void readY();
 void readQ();
+
+void overwriteAns();
 void printAns();
 
 int randInt(int);
 
 void runJudger();
+int runBenchmark();
 
-int T, M, N, Q, D[9000][40], C[200], Y[40][200], X[9000][40][200];
+void shuffleMMask();
+void shuffleNMask();
+
+int T, M, N, Q, D[9000][40], C[200], Y[40][200], X[9000][40][200],
+    Xans[9000][40][200], ansBenchmark = 1e9;
+int MMask[40], NMask[200];
 string clientName[40], siteName[200];
 
-int main() {
-  srand(time(nullptr));
-  readDemand();
-  readCap();
-  readY();
-  readQ();
+void assign() {
   for (int t = 1; t <= T; ++t) {
     int cap[200];
     for (int j = 1; j <= N; ++j)
       cap[j] = C[j];
-    for (int i = 1; i <= M; ++i) {
+    shuffleMMask();
+    for (int imask = 1; imask <= M; ++imask) {
+      int i = MMask[imask];
       int w = D[t][i];
       int Scap = 0;
       for (int j = 1; j <= N; ++j)
         Scap += cap[j];
-      for (int j = 1; j <= N; ++j) {
+      shuffleNMask();
+      for (int jmask = 1; jmask <= N; ++jmask) {
+        int j = NMask[jmask];
         if (Y[i][j] < Q) {
-          int x = min(cap[j], min(w, randInt(2.0 * cap[j] / Scap * w)));
+          int x = min(cap[j], min(w, randInt(2.0 * cap[j] * w / Scap)));
           X[t][i][j] += x;
           Scap -= cap[j];
           w -= x;
@@ -46,12 +59,10 @@ int main() {
         for (int j = 1; j <= N; ++j)
           Scap += cap[j];
         for (int j = 1; j <= N; ++j) {
-          // cout << j << endl;
           if (Y[i][j] < Q) {
             // FIXME: this is a bug.
             // int x = min(cap[j], min(w, randInt(100.0 * cap[j] / Scap * w)));
             int x = min(cap[j], min(w, randInt(100000)));
-            // cout << x << endl;
             X[t][i][j] += x;
             Scap -= cap[j];
             w -= x;
@@ -61,8 +72,32 @@ int main() {
       }
     }
   }
+}
 
-  runJudger();
+int main() {
+  srand(time(nullptr));
+  readDemand();
+  readCap();
+  readY();
+  readQ();
+  for (int i = 1; i <= M; ++i)
+    MMask[i] = i;
+  for (int j = 1; j <= N; ++j)
+    NMask[j] = j;
+
+  for (int i = 1; i <= 100; ++i) {
+    memset(X, 0, sizeof(X));
+    assign();
+    int curBenchmark = runBenchmark();
+    cout << "Round " << i << " Benchmark: " << curBenchmark << endl;
+    if (curBenchmark < ansBenchmark) {
+      ansBenchmark = curBenchmark;
+      overwriteAns();
+    }
+  }
+
+  // runJudger();
+  cout << "\nAns Benchmark: " << ansBenchmark << endl;
 
   printAns();
   return 0;
@@ -71,7 +106,10 @@ int main() {
 void readDemand() {
   fstream fin;
   stringstream ss;
-  fin.open("./data/demand.csv", ios::in);
+  if (LOCAL_DEBUG)
+    fin.open("./data/demand.csv", ios::in);
+  else
+    fin.open("/data/demand.csv", ios::in);
   string line, element;
   getline(fin, line);
   if (line[line.size() - 1] == '\r')
@@ -100,7 +138,10 @@ void readDemand() {
 void readCap() {
   fstream fin;
   stringstream ss;
-  fin.open("./data/site_bandwidth.csv", ios::in);
+  if (LOCAL_DEBUG)
+    fin.open("./data/site_bandwidth.csv", ios::in);
+  else
+    fin.open("/data/site_bandwidth.csv", ios::in);
   string line, element;
   getline(fin, line);
   if (line[line.size() - 1] == '\r')
@@ -122,7 +163,10 @@ void readCap() {
 void readY() {
   fstream fin;
   stringstream ss;
-  fin.open("./data/qos.csv", ios::in);
+  if (LOCAL_DEBUG)
+    fin.open("./data/qos.csv", ios::in);
+  else
+    fin.open("/data/qos.csv", ios::in);
   string line, element;
   getline(fin, line);
   if (line[line.size() - 1] == '\r')
@@ -145,7 +189,10 @@ void readY() {
 void readQ() {
   fstream fin;
   stringstream ss;
-  fin.open("./data/config.ini", ios::in);
+  if (LOCAL_DEBUG)
+    fin.open("./data/config.ini", ios::in);
+  else
+    fin.open("/data/config.ini", ios::in);
   string line, element;
   getline(fin, line);
   getline(fin, line);
@@ -158,24 +205,24 @@ void readQ() {
   fin.close();
 }
 
-int randInt(int upper) { return rand() % (upper + 1); }
-
 void printAns() {
-
   fstream fout;
-  fout.open("./output/solution.txt", ios::out);
+  if (LOCAL_DEBUG)
+    fout.open("./output/solution.txt", ios::out);
+  else
+    fout.open("/output/solution.txt", ios::out);
   for (int t = 1; t <= T; ++t) {
     for (int i = 1; i <= M; ++i) {
       fout << clientName[i] << ":";
       bool isFirst = true;
       for (int j = 1; j <= N; ++j) {
-        if (X[t][i][j] == 0)
+        if (Xans[t][i][j] == 0)
           continue;
         if (isFirst)
           isFirst = false;
         else
           fout << ",";
-        fout << "<" << siteName[j] << "," << X[t][i][j] << ">";
+        fout << "<" << siteName[j] << "," << Xans[t][i][j] << ">";
       }
       if (t == T && i == M)
         continue;
@@ -241,3 +288,30 @@ void runJudger() {
   else
     cout << "Passed Constraint 4." << endl;
 }
+
+int runBenchmark() {
+  int res = 0, w[9000], pos = ceil(0.95 * T);
+  for (int j = 1; j <= N; ++j) {
+    for (int t = 1; t <= T; ++t) {
+      w[t] = 0;
+      for (int i = 1; i <= M; ++i)
+        w[t] += X[t][i][j];
+    }
+    sort(w + 1, w + T + 1);
+    res += w[pos];
+  }
+  return res;
+}
+
+void shuffleMMask() { random_shuffle(MMask + 1, MMask + M + 1); }
+
+void shuffleNMask() { random_shuffle(NMask + 1, NMask + N + 1); }
+
+void overwriteAns() {
+  for (int t = 1; t <= T; ++t)
+    for (int i = 1; i <= M; ++i)
+      for (int j = 1; j <= N; ++j)
+        Xans[t][i][j] = X[t][i][j];
+}
+
+int randInt(int upper) { return rand() % (upper + 1); }
